e38ebbfa0d1d95b14774c2138f27fca1749e52c1
mmz1380 <mohammad.mesbah.mz.1380@gmail.com>
2023-03-27 23:22:19 +0430

    init-pre

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from IPython.display import display
 
-
-
-this is main file of the newruz HWs
-
-
-
-
-
-
-
-
-
-
-
-"""
+# header_name = ["Elevation","Aspect","Slope","Horizontal_Distance_To_Hydrology","Vertical_Distance_To_Hydrology","Horizontal_Distance_To_Roadways","Hillshade_9am","Hillshade_Noon","Hillshade_3pm","Horizontal_Distance_To_Fire_Points"]
+data = pd.read_csv("covtype.data")
+display(data.shape)
